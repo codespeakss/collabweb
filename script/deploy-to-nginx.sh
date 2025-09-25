880e6df58ea697b485a83c7b8e3bd65699e2e0f0
codespeakss <dd@ci.com>
2025-09-25 10:49:32 +0800

    script: deploy-to-nginx.sh

---
+++ script/deploy-to-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 构建并发布前端产物到 nginx 目录
+
+# 1. 构建前端
+npm run build
+
+# 2.  nginx 目标目录
+NGINX_DIR="67_root:/var/www/dev.bewantbe.com"
+
+# 3. 拷贝构建产物到 nginx 目录
+scp -r dist/* "$NGINX_DIR"/
+
+echo "发布完成，前端已部署到 $NGINX_DIR"
